c27e041c004e7856d78639945835e14f898e8f34
Alexander Moskalenko <alexmoss09@gmail.com>
2016-01-22 17:21:17 +0200

    create migration table serial. Create Model Serial

---
+++ app/Serial.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Serial extends Model implements SluggableInterface
+{
+    use SluggableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'country', 'production', 'producer', 'actors', 'description', 'images', 'released'
+    ];
+
+
+    protected $sluggable = [
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    ];
+
+}
---
+++ database/migrations/2016_01_22_151920_create_serials_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSerialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('serials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('country')->nullable();
+            $table->string('production')->nullable();
+            $table->string('producer')->nullable();
+            $table->string('actors')->nullable();
+            $table->string('description');
+            $table->string('images')->nullable();
+            $table->string('released')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('serials');
+    }
+}
